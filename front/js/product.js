63124fe0732d9bcf7940e93e04f7a92c8c016feb
Quentin <charton.nw@gmail.com>
2022-04-29 15:49:45 +0200

    Product voir comment importer l'image et finir js

--- front/js/product.js
+++ front/js/product.js
@@ -1,28 +1,43 @@
 
+// pour importer l'image, (ça marche po) oscour
+	let img = document.createElement('img');
+	img.src = "./images/logo.png";
+	let block = document.getElementsByClassName('item__img');
+	// block.classList.add('item__img'); Il faut que j'add la class
+	block.appendChild = img;
+	//le log est bon pourtant on a bien l'elt img
+	console.log(img)
+
+//récupérer les paramètres dans l'url
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const productId = urlParams.get('id');
 
+//Appel a l'API avec "id" en paramètre de function
 let url = `http://localhost:3000/api/products/${productId}`;
 
 fetch(url).then(response => response.json().then((data) => {
 
+	document.getElementById('title').innerHTML = data.name;
+	document.getElementById('price').innerHTML = data.price;
+	document.getElementById('description').innerHTML = data.description;
+	document.getElementsByClassName('item__image').innerHTML = data.imageUrl;
 
-		document.getElementById('title').innerHTML=data.name;
-		document.getElementById('price').innerHTML=data.price;
-		document.getElementsByClassName('item__image').innerHTML=data.imageUrl;
-		document.getElementById('description').innerHTML=data.description;
+	let colorsNode = document.getElementById('colors');
+	for (let color of data.colors) {
+		console.log(color);
+		let addOption = document.createElement('option');
+		addOption.text = color;
+		addOption.value = color;
+		colorsNode.add(addOption);
+	}
+}));
 
-		let colorsNode=document.getElementById('colors');
-		for (let color of data.colors){
-			console.log(color);
-			var addedOption = document.createElement('option');
-			addedOption.text = color;
-			addedOption.value = color;
-			colorsNode.add(addedOption);
-		}
+//Écouteur d'évènement sur la soumission du panier
+let button = document.querySelector('#addToCart');
 
-	}));
-
+button.addEventListener('click', function (){
+	console.log(button)
+})
 
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,21 +1,22 @@
 let url = `http://localhost:3000/api/products`;
 
-fetch(url).then( response => response.json().then((data) => {
-	console.log(data);
-	let html='';
-	data.forEach(function(item) {
-		html+=`<a href="product.html?id=${item._id}">
+//
+fetch(url).then(response => response.json().then((data) => {
+		console.log(data);
+		let html = '';
+		data.forEach(function (item) {
+			html += `<a href="product.html?id=${item._id}">
 			<article>
 				<img src="${item.imageUrl}" alt="${item.altTxt}">
 					<h3 class="productName">${item.name}</h3>
 					<p class="productDescription">${item.description}</p>
 			</article>
 		</a>`;
-	},  this);
+		}, this);
 
-	document.getElementById('items').innerHTML=html;
+		document.getElementById('items').innerHTML = html;
 
-})
+	})
 ).catch(err => console.log('Erreur : ' + err));
 
 
